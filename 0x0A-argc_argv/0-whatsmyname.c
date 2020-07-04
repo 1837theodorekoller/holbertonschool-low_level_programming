b53d369bf67047a745fb99d6caa972d2119566a3
1837theodorekoller <66273693+1837theodorekoller@users.noreply.github.com>
2020-07-04 14:07:43 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - entry point
- * @argc: number of arguments for the function
- * @argv: arguments for the function
- *
- * Return: always 0.
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
  */
-
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
+
 	return (0);
 }
